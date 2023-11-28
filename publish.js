22bc9b64b0a76d7e7362f5e48dbc7bf41f37d3a0
Li Ziang <92004310+Anker-Lee@users.noreply.github.com>
2023-11-28 09:54:43 +0800

    Add files via upload

---
+++ publish.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // 读取并响应 system.html 文件
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url.startsWith('/js/')) {
+    // 处理对 /js/ 路径下文件的请求
+    const filePath = path.join(__dirname, req.url);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+        const contentType = getContentType(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+const port = 3001;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+// 辅助函数，获取文件的 Content-Type
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.js':
+      return 'text/javascript';
+    case '.css':
+      return 'text/css';
+    // 添加其他可能的文件类型
+    default:
+      return 'text/plain';
+  }
+}
